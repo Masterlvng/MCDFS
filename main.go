1a6756d67b8fa5b871f7d9816f479feb405827b0
masterlvng <masterlvng@gmail.com>
2014-05-08 18:03:22 +0800

    second commit

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"command"
+	"flag"
+	"fmt"
+	"github.com/goraft/raft"
+	"math/rand"
+	"os"
+	"server"
+	"time"
+)
+
+var host string
+var port int
+var join string
+
+var vlocation string
+
+func init() {
+	flag.StringVar(&host, "h", "localhost", "hostname")
+	flag.IntVar(&port, "p", 4001, "port")
+	flag.StringVar(&join, "join", "", "host:port of leader to join")
+	flag.StringVar(&vlocation, "vl", "", "where to store volume")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [arguments] <data-path> \n", os.Args[0])
+		flag.PrintDefaults()
+	}
+}
+
+func main() {
+	flag.Parse()
+	rand.Seed(time.Now().UnixNano())
+	raft.RegisterCommand(&command.WriteCommand{})
+	if flag.NArg() == 0 {
+		flag.Usage()
+	}
+	path := flag.Arg(0)
+	os.MkdirAll(path, 0744)
+	var dirname []string
+	dirname = append(dirname, vlocation)
+	s := server.New(path, host, port, dirname)
+	s.ListenAndServe(join)
+}
